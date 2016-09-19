c18985a66006dd114a417538404f670e1c4257e9
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-09-19 00:47:02 -0700

    edit callback uri to class method

--- app/models/instagramapi.rb
+++ app/models/instagramapi.rb
@@ -1,6 +1,10 @@
 class Instagramapi
-	
+
 	def self.endpoint_maker(endpoint_to_make, sesh_token)
 		url = %W{#{Instagram.endpoint} #{endpoint_to_make} access_token= #{sesh_token}}.join()
+	end
+
+	def self.callback_uri
+		Rails.application.secrets.INSTA_CALLBACK_URL
 	end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,7 +1,8 @@
 Instagram.configure do |config|
   config.client_id = Rails.application.secrets.INSTA_CLIENT_ID
   config.client_secret = Rails.application.secrets.INSTA_CLIENT_SECRET
+  binding.pry
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
-CALLBACK_URL = Rails.application.secrets.INSTA_CALLBACK_URL
+# @CALLBACK_URL = Rails.application.secrets.INSTA_CALLBACK_URL
