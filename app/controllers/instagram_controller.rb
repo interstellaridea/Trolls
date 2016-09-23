4bec18b693118babb3e00fe107b92c88331bd447
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-09-22 23:08:27 -0700

    add params to insta callback controller method

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -5,13 +5,15 @@ require 'ostruct'
 class InstagramController < ApplicationController
 
 	def connect
-		redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL, :scope => 'public_content')
+		#binding.pry
+		#redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL, :scope => 'public_content')
+		redirect_to Instagram.authorize_url(:client_secret => Instagram.client_secret, :redirect_uri => CALLBACK_URL, :scope => 'public_content')
 	end
 
 	def callback
 		response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
 		session[:access_token] = response.access_token
-		
+
 		redirect_to :controller => 'welcome', :action => 'show'
 	end
 end
