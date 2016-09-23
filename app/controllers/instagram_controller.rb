db07108ed696cdedd51c19daa5e28b0914193a43
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-09-22 20:28:31 -0700

    revert routes, add :code to session

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -11,6 +11,7 @@ class InstagramController < ApplicationController
 	def callback
 		response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
 		session[:access_token] = response.access_token
+		session[:code] = response.code
 
 		redirect_to :controller => 'welcome', :action => 'show'
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/show', to: 'welcome#show'
   get 'oauth/connect', to: 'instagram#connect', as: 'insta_connect'
-  get 'oauth/instagram/callback', to: 'instagram#callback', via: [:get, :post]
+  get 'oauth/instagram/callback', to: 'instagram#callback'
 end
