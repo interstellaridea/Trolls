c73255941832fd441bd16983ca519d9a7ccbef64
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-09-22 22:12:04 -0700

    revert insta controller, edit welcompage

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -12,8 +12,6 @@ class InstagramController < ApplicationController
 		response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
 		session[:access_token] = response.access_token
 		
-		session[:code] = response.code
-
 		redirect_to :controller => 'welcome', :action => 'show'
 	end
 end
