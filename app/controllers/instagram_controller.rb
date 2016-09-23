d0730778f63b905042c224f3ddaf24313c61ad68
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-09-22 23:15:45 -0700

    edit .pictures class and clean-up insta#connect method

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -5,9 +5,7 @@ require 'ostruct'
 class InstagramController < ApplicationController
 
 	def connect
-		#binding.pry
-		#redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL, :scope => 'public_content')
-		redirect_to Instagram.authorize_url(:client_secret => Instagram.client_secret, :redirect_uri => CALLBACK_URL, :scope => 'public_content')
+		redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL, :scope => 'public_content')
 	end
 
 	def callback
